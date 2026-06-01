fc260f57a8ec88297c62bc0ae20e5c26388a5460
scttfrdmn <3011922+scttfrdmn@users.noreply.github.com>
2026-06-01 12:28:41 -0700

    bake: fix pip3 → python3 -m pip (AL2023 has no pip3 symlink)

--- build_ami.py
+++ build_ami.py
@@ -117,7 +117,7 @@ tar -xzf k2_pluspf_16_GB_20260226.tar.gz
 rm k2_pluspf_16_GB_20260226.tar.gz
 
 # --- MetaPhlAn 4 + marker gene database ------------------------------------
-pip3 install metaphlan
+python3 -m pip install metaphlan
 # Pre-download the MetaPhlAn marker gene database (~2 GB)
 mkdir -p /opt/databases/metaphlan
 metaphlan --install --bowtie2db /opt/databases/metaphlan \\
